8c473490ea06bd2e655a580392d3d58af58b01b5
Dinne Kopelevich <dinne22@hotmail.com>
2022-07-05 17:50:16 -0600

    Finish our first tests classwork

--- src/Box.js
+++ src/Box.js
@@ -1,4 +1,14 @@
+class Box {
+  constructor(height = 100, width = 100) {
+    this.height = height || 100;
+    this.width = width || 100;
+  }
+  calculateArea() {
+    return this.height * this.width;
+  }
+  increment(num, val) {
+    return this[val] + num;
+  }
+}
 
-
-
-// module.exports = Box;
+module.exports = Box;
--- test/Box-test.js
+++ test/Box-test.js
@@ -1,3 +1,39 @@
-// const chai = require('chai');
-// const expect = chai.expect;
-// const Box = require('../src/Box');
+const chai = require("chai");
+const expect = chai.expect;
+const Box = require("../src/Box");
+
+describe("Box", function () {
+  it("should return true", function () {
+    expect(true).to.equal(true);
+  });
+
+  it("should have a default height and a width", function () {
+    var box = new Box();
+
+    expect(box.height).to.equal(100);
+    expect(box.width).to.equal(100);
+  });
+
+  it("should be able to take in any height and width", function () {
+    var box = new Box(25, 50);
+
+    expect(box.height).to.equal(25);
+    expect(box.width).to.equal(50);
+  });
+
+  it("should calculate area of the box", function () {
+    var box = new Box(10, 10);
+
+    expect(box.calculateArea()).to.equal(100);
+  });
+
+  it("should should increment the height and width of the box with a value", function () {
+    var box = new Box(10, 10);
+
+    box.increment(10, "height");
+    box.increment(10, "width");
+
+    expect(box.increment(10, "height")).to.equal(20);
+    expect(box.increment(10, "width")).to.equal(20);
+  });
+});
